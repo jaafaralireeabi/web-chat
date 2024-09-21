9a2adfdc8f9ed55d1b5208c3939265dcb5babb0b
jaafaralireeabi <159414760+jaafaralireeabi@users.noreply.github.com>
2024-09-21 10:14:27 +0300

    Update page.js

--- app/[chat]/page.js
+++ app/[chat]/page.js
@@ -3,34 +3,35 @@ import "./chat.css"
 import { faRightFromBracket, faPlus } from "@fortawesome/free-solid-svg-icons";
 import Modal from "../components/modal";
 import DarkMode from "../components/darkMode";
-import DbConnect from "../../scripts/seed";
-import { pool } from "../../scripts/seed";
 
-export default function Chat({ imgUrl="images/profile1.jpg"}) {
-    return(
+export default function ChatPage({ params }) {
+    const { chatId } = params; // if you need to extract chatId or other params
+    const imgUrl = "images/profile1.jpg"; // default image URL
+
+    return (
         <html suppressHydrationWarning>
             <body dir="rtl">
-            <Modal></Modal>
-            <nav>
-                <div className="profile">
-                    <img src={imgUrl} alt="no image"></img>
-                    <div className="uList">
-                        <DarkMode />
-                        <div className="logOut">
-                            <a href="./">
-                                <FontAwesomeIcon icon={faRightFromBracket} />
-                            </a>
+                <Modal />
+                <nav>
+                    <div className="profile">
+                        <img src={imgUrl} alt="Profile" />
+                        <div className="uList">
+                            <DarkMode />
+                            <div className="logOut">
+                                <a href="./">
+                                    <FontAwesomeIcon icon={faRightFromBracket} />
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="chats">
-                    <div className="newChat">
-                        <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-                        <p>محادثة جديدة</p>
+                    <div className="chats">
+                        <div className="newChat">
+                            <FontAwesomeIcon icon={faPlus} />
+                            <p>محادثة جديدة</p>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </body>
+                </nav>
+            </body>
         </html>
     );
 }
